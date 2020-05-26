d93490501eab1d20fff7b4b829dd69c5c140804d
supergirl-lml <Liumeimeijam@163.com>
2020-05-26 22:03:19 +0800

    2020.04.28第十周课堂实验

---
+++ Lib0/Lib0/w32.cpp
@@ -0,0 +1,8 @@
+#include"stdafx.h"
+#include"w32.h"
+char* pchar() {
+	return "这是从静态库获得的字符串";
+}
+int  F1::GetA() {
+	return a + 1;
+}
---
+++ Lib0/Lib0/w32.h
@@ -0,0 +1,8 @@
+#pragma once
+char* pchar();//函数声明
+class F1 {
+	int a;
+public:
+	F1() { a = 100; }
+	int GetA();
+};//类声明
